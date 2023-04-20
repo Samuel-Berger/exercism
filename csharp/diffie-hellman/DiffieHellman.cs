0348bfeef0a46e9475c2518ee28625fd4796122d
Samuel Berger <s@familjenberger.com>
2023-04-20 23:23:49 +0200

    Working solution with explicit casts.

--- csharp/diffie-hellman/DiffieHellman.cs
+++ csharp/diffie-hellman/DiffieHellman.cs
@@ -5,16 +5,35 @@ public static class DiffieHellman
 {
     public static BigInteger PrivateKey(BigInteger primeP) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        var rand = new Random();
+        return rand.Next(1, (int)primeP);
     }
 
     public static BigInteger PublicKey(BigInteger primeP, BigInteger primeG, BigInteger privateKey) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        // A = gᵃ mod p
+        double power =  Math.Pow((double)primeG, (double)privateKey);
+        double modulo  = power % (double)primeP;
+        return (BigInteger)modulo;
     }
 
     public static BigInteger Secret(BigInteger primeP, BigInteger publicKey, BigInteger privateKey) 
     {
-        throw new NotImplementedException("You need to implement this function.");
+        /*
+        Alice and Bob exchange public keys.
+        Alice calculates secret key s.
+        
+            s = Bᵃ mod p
+        
+        Bob calculates
+        
+            s = Aᵇ mod p
+        
+        The calculations produce the same result!
+        Alice and Bob now share secret s.
+        */
+        double power =  Math.Pow((double)publicKey, (double)privateKey);
+        double modulo = power % (double)primeP;
+        return (BigInteger)modulo;
     }
 }
