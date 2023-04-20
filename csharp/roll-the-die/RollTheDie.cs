f34633b1b0e9fade88450eb6bed525b5af8b9695
Samuel Berger <s@familjenberger.com>
2023-04-20 21:19:39 +0200

    Working solution

--- csharp/roll-the-die/RollTheDie.cs
+++ csharp/roll-the-die/RollTheDie.cs
@@ -1,14 +1,14 @@
-using System;
-
 public class Player
 {
     public int RollDie()
     {
-        throw new NotImplementedException("Please implement the Player.RollDie() method");
+        var random = new System.Random();
+        return random.Next(1, 18);
     }
 
     public double GenerateSpellStrength()
     {
-        throw new NotImplementedException("Please implement the Player.GenerateSpellStrength() method");
+        var random = new System.Random();
+        return random.NextDouble();
     }
 }
